82f37e2a5956fbaa295bc5113d66ba7b2b1b3f23
Aliyeva Sabina <sabinaaliyeva-code>
2026-05-12 02:39:42 +0400

    changes

--- JS/Services/api.js
+++ JS/Services/api.js
@@ -2,7 +2,7 @@ export async function getProduct(){
 
 
 try{
-    let response = await fetch("../../data.json");
+    let response = await fetch("./data.json");
     let data= await response.json();
     return data;
 
--- JS/Utils/cart.js
+++ JS/Utils/cart.js
@@ -77,6 +77,8 @@ export async function renderCart(){
     
     
     });
+
+    
    
         
         
